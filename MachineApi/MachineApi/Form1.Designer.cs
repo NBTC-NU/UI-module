2f9f1a5b9b722f7544da6b2c12d73ffb2d98d2ee
Sahakorn Buangam <sahakorn.new@outlook.com>
2014-11-18 12:20:17 +0700

    Machine API ( Old Version) (No Acknowledge)
    This is a Machine API using a C# for Connect a DB and Send DATA to
    Serial port

---
+++ MachineApi/MachineApi/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace MachineApi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.fontDialog1 = new System.Windows.Forms.FontDialog();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.OpenSerial = new System.Windows.Forms.Button();
+            this.Terminate = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(36, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(264, 20);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Read DATA";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(347, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(30, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "From";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(445, 31);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Date ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(539, 31);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Time";
+            // 
+            // OpenSerial
+            // 
+            this.OpenSerial.Location = new System.Drawing.Point(36, 57);
+            this.OpenSerial.Name = "OpenSerial";
+            this.OpenSerial.Size = new System.Drawing.Size(75, 23);
+            this.OpenSerial.TabIndex = 5;
+            this.OpenSerial.Text = "OpenSerial";
+            this.OpenSerial.UseVisualStyleBackColor = true;
+            this.OpenSerial.Click += new System.EventHandler(this.OpenSerial_Click);
+            // 
+            // Terminate
+            // 
+            this.Terminate.Location = new System.Drawing.Point(117, 57);
+            this.Terminate.Name = "Terminate";
+            this.Terminate.Size = new System.Drawing.Size(75, 23);
+            this.Terminate.TabIndex = 6;
+            this.Terminate.Text = "Terminate.";
+            this.Terminate.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(634, 90);
+            this.Controls.Add(this.Terminate);
+            this.Controls.Add(this.OpenSerial);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FontDialog fontDialog1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button OpenSerial;
+        private System.Windows.Forms.Button Terminate;
+        private System.IO.Ports.SerialPort serialPort1;
+    }
+}
+
---
+++ MachineApi/MachineApi/Form1.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+namespace MachineApi
+{
+    public partial class Form1 : Form
+    {
+      // Define a Variable for uses.
+        private Timer timer;
+        private byte Num_Count = 5;
+        private String Channel, Date, Time,User;
+        private String TempChannel = "";
+        public Form1()
+        {
+            InitializeComponent();
+
+            //Set a timer Overflow every 1000 ms
+            timer = new Timer();
+            timer.Interval = 1000;
+
+            //For Timer Tick
+            timer.Tick += TimerTicker;
+            
+        }
+        //Create a Timer Ticker Function.
+        void TimerTicker(object sender, EventArgs e)
+        {
+            // Count Down for 5 Second
+            Num_Count--;
+
+            if (Num_Count == 0)
+            {
+                //Restart Count.
+                Num_Count = 5;
+
+                //If Count Down = 5 Second 
+                //Connect To a database.
+                ConnectDatabase();
+            }
+        }
+        private void OpenSerialPort()
+        {
+            if (serialPort1.IsOpen == false)
+            {
+                // Open serial port
+                serialPort1.Open();
+
+                //Send Data.
+                SendDatas();
+
+                // Terminate a Serial port.
+                serialPort1.Close();
+            }
+
+            else
+            {
+               // Serial Port is Ready to Send a Data.
+            }
+
+        }
+        private void SendDatas()
+        {
+            int DataLength = Channel.Length;
+            for (int i = 0; i < DataLength; i++)
+            {
+                if (serialPort1.IsOpen == true)
+                {
+                    serialPort1.Write(Channel[i].ToString());
+                }
+
+                // Delay for Microcontroller receive Data
+                System.Threading.Thread.Sleep(500);
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Do something 
+        }
+
+        private void OpenSerial_Click(object sender, EventArgs e)
+        {
+            timer.Start();
+        }
+
+        private void ConnectDatabase()
+        {
+            //using MySql.Data.MySqlClient; 
+            // Connect to database and Uses a Sql Command to qeury.
+            string stConnect = @"server=localhost;database=channel;userid=root;password=korn1993";
+
+            //Server and DB Connnect.
+            MySqlConnection ConnectDB = new MySqlConnection(stConnect);
+            try
+            {
+                ConnectDB.Open();
+                MySqlCommand cmd = new MySqlCommand("SELECT * FROM channel", ConnectDB);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                // Read a data from database. 
+                while (dataReader.Read())
+                {
+                    Channel = dataReader["channel"].ToString();
+                    Date = dataReader["Date"].ToString();
+                    Time = dataReader["time"].ToString();
+                    User = dataReader["Userid"].ToString();
+                    break;
+                }
+
+
+                //close Data Reader
+                dataReader.Close();
+
+                //close Connection
+                ConnectDB.Close();
+
+                /* CHECK IF DATA IS SAME AT THE TIME
+                 * If First Time receive A TempChannel is No data
+                 * And We push a Data (Channel) to TempChannel, So TempChannel = Channel.
+                 * The next time we can Check if Data same, Donothing.
+                */
+
+                if (Channel == TempChannel)
+                {
+                    TempChannel = Channel;
+
+                    //Show in label
+                    label2.Text = User;
+
+                    label3.Text = Date;
+                    label4.Text = Time;
+                }
+                else
+                {
+                    
+                    TempChannel = Channel;
+
+                    // Show a channel From database.
+                    textBox1.Text = Channel.ToString();
+
+                    //Show in label
+                    label2.Text = User;
+                    label3.Text = Date;
+                    label4.Text = Time;
+
+                    //Open A serial port. 
+                    // Ready to send a Data. 
+                    OpenSerialPort();
+
+                }
+            }
+            catch
+            {
+                // Catch Error.
+                MessageBox.Show("Error Connection.");
+            }
+
+        }
+    }
+}
